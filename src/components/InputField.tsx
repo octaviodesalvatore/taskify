4518a026dd7d6bead339f78c47209ecd0b1e3868
Octavio <octaviodesalvatore@gmail.com>
2022-05-11 11:28:08 -0300

    Input Field logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
       <input
         type="input"
         className="input__box"
         placeholder="Agregar una tarea"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
       ></input>
       <button className="input_submit" type="submit">
         Go
